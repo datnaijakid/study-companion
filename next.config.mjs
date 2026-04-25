1fafa517c75f3dc9e434bf3aba960c8ba48fe4b9
datnaijakid <jpakhator@upei.ca>
2026-04-24 23:08:22 -0300

    updated next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 1. Prisma/Database needs to be externalized
-  serverExternalPackages: ['@prisma/client', 'pg'],
-
-  // 2. The NEW way to configure Turbopack in v16
+  serverExternalPackages: ['@prisma/client'],
+  
   turbopack: {
     resolveAlias: {
       '.prisma/client/default': './node_modules/.prisma/client/default.js',
